4d560cf132e5be0cb93def9a7af4cb457982b615
tranjas <jasoncongtranjob@gmail.com>
2023-08-01 17:17:19 -0700

    Project tab

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,25 +4,71 @@ import Map from '../assets/Map.jpg'
 const Project = () => {
   return (
     <div name ='work' className='bg-[#0a192f] w-full md:h-screen text-gray-300'>
-        <div className='max-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects and Internship</p>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
                 <p className='py-6'>// Check out some of my recent projects</p>
 
-                <div style={{backgroundImage: `url(${Map})`}}
-                className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
+                {/* container */}
+                <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
+                    <div
+                    style={{backgroundImage: `url(${Map})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
                                 Husky Map
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href=''>
-                                    <button></button>
+                                <a href='https://github.com/tranjas/Husky-Map'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>GitHub</button>
                                 </a>
-                                <a href=''>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div 
+                    style={{backgroundImage: `url(${Map})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Portfolio Website
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://github.com/tranjas/Husky-Map'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>GitHub</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                    style={{backgroundImage: `url(${Map})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Snake and Apples
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://github.com/tranjas/Husky-Map'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>GitHub</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                    style={{backgroundImage: `url(${Map})`}} 
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Audio Visualizer
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='https://github.com/tranjas/Husky-Map'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>GitHub</button>
                                 </a>
                             </div>
