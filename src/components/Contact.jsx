91f1d36fc4fcb7e94086544f788a04a4d9695fc5
tranjas <jasoncongtranjob@gmail.com>
2023-08-01 19:10:42 -0700

    email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - Jasoncongtranjob@gmail.com</p>
             </div>
             <input  className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] ' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2'name ="message" placeholder= 'Message' rows="10"></textarea> 
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' name ="message" placeholder='Message' rows="10"></textarea>
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'> Let's Connect</button>
         </form>
     </div>
